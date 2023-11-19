77ab29c8cf0702029e1ce83fd4242e2e83c5100e
Ekaterina <ekaterina.fedotova@temedica.com>
2023-11-19 22:21:35 +0100

    add getProductById and separate into 2 different handlers

--- getProductsList.js
+++ getProductsList.js
@@ -5,7 +5,7 @@ const { productList } = require("./dummy-data/productList");
 module.exports.getProductsList = async (event) => {
   return {
     statusCode: 200,
-    body:  JSON.stringify(productList)
+    body: JSON.stringify(productList)
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
